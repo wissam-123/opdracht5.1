5edc4018b827616183246722d2808573feca8fa4
wissam-123 <144032700+wissam-123@users.noreply.github.com>
2023-10-04 12:05:28 +0200

    Add files via upload

---
+++ javascript.5.1/opdracht 5.1 javascript/opmaak.js
@@ -0,0 +1,8 @@
+// Selecteer het element met het ID 'mijnTitel'
+var titelElement = document.getElementById('myhead');
+
+// Pas de kleur aan van het element naar blauw
+titelElement.style.color = 'red';
+
+// Pas de tekst aan van het element
+titelElement.innerText = 'Dit is een aangepaste zin';
